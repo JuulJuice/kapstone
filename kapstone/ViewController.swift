02e12554ba288feb8780c9e0fb381f401d78b182
Julian <taz38877@msn.com>
2021-12-03 14:12:12 -0500

    iok

--- kapstone/ViewController.swift
+++ kapstone/ViewController.swift
@@ -8,12 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var forgotUsernameButton: UIButton!
+    @IBOutlet weak var forgotPasswordButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
-
